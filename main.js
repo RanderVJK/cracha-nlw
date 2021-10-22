2fc11febf676784718202af62f765c724c73a6fc
Vanderlei Kempf <dev.vjk@gmail.com>
2021-10-21 22:26:43 -0300

    added JS for exchanging information in HTML

---
+++ main.js
@@ -0,0 +1,42 @@
+/*       function showMeSomething() {
+  alert(links.github);
+} */
+
+const linksSocialMedia = {
+  github: 'RanderVJK',
+  youtube: 'UCg1V3hn7jKs_xwG18ZfpVkQ',
+  facebook: 'vanderlei.kempf/',
+  instagram: 'vanderleikempf/',
+  twitter: 'KempfVanderlei'
+}
+/* showMeSomething(); */
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+
+    //alert(li.children[0].href)
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  //alert(url)
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
